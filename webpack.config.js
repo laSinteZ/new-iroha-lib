203d2b8dfedac5d0253997742a20676ec1fb99b1
Vyacheslav Bikbaev <la.sintez@gmail.com>
2018-07-26 12:22:16 +0300

    feat: initial webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin()]
+}
